4ae780db827efccfc15cef0e8b9fe9e6897a4273
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-10-18 09:33:59 +0200

    Disabled search tests

--- cypress/integration/search.test.js
+++ cypress/integration/search.test.js
@@ -1,40 +1,40 @@
 /// <reference types="Cypress" />
 
-context('Search', () => {
-    before(() => {
-      cy.visit('/')
-    })
+// context('Search', () => {
+//     before(() => {
+//       cy.visit('/')
+//     })
 
-    it('Should open searchbox', ()=> {
-        cy.get('nav').findByText('Search').click().get('#search-input').should('be.visible')
-    })
+//     it('Should open searchbox', ()=> {
+//         cy.get('nav').findByText('Search').click().get('#search-input').should('be.visible')
+//     })
 
-    const expectedPostTitle = 'Firmware Update Notifications for My Asus Router';
-    it(`Should find post with title '${expectedPostTitle}' search for "asus firmware"`, ()=> {
-        cy.get('#search-input')
-          .wait(5000) // give some time for index file to download
-          .type('asus firmware')
-          .get('#search-output .result-list')
-          .within(() => {
-            cy.findByText(expectedPostTitle)
-            .should('be.visible')
-          })
-    })
+//     const expectedPostTitle = 'Firmware Update Notifications for My Asus Router';
+//     it(`Should find post with title '${expectedPostTitle}' search for "asus firmware"`, ()=> {
+//         cy.get('#search-input')
+//           .wait(5000) // give some time for index file to download
+//           .type('asus firmware')
+//           .get('#search-output .result-list')
+//           .within(() => {
+//             cy.findByText(expectedPostTitle)
+//             .should('be.visible')
+//           })
+//     })
 
-    it(`Should navigate to post when clicked`, ()=> {
-        cy.get('#search-output .result-list')
-          .within(() => {
-            cy.findByText(expectedPostTitle)
-            .click()
-            .url()            
-            .should('equal', Cypress.config().baseUrl + `/firmware-update-notifications-for-my-asus-router/`)
-          })
-    })
+//     it(`Should navigate to post when clicked`, ()=> {
+//         cy.get('#search-output .result-list')
+//           .within(() => {
+//             cy.findByText(expectedPostTitle)
+//             .click()
+//             .url()
+//             .should('equal', Cypress.config().baseUrl + `/firmware-update-notifications-for-my-asus-router/`)
+//           })
+//     })
 
-    it('Should close on ESC key', ()=> {
-        cy.get('nav').findByText('Search').click().get('#search-input')
-            .type('{esc}')
-            .get('#search-input').should('not.be.visible')
+//     it('Should close on ESC key', ()=> {
+//         cy.get('nav').findByText('Search').click().get('#search-input')
+//             .type('{esc}')
+//             .get('#search-input').should('not.be.visible')
 
-    })    
-})
+//     })
+// })
