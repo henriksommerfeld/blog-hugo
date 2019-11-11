52a6866dd1a094ee54063d6bec90bf55e905ccb6
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-11-11 22:39:53 +0100

    Code expander done

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import $ from 'jquery';
 import 'lazysizes';
 import ThemeSwitcher from './theme-switcher'
 import Hamburger from './hamburger';
@@ -9,7 +8,7 @@ import Search from './search';
 import CodeExpanded from './code-expanded';
 import EmailCaptcha from './e-mail'
 
-$(() => {
+document.addEventListener("DOMContentLoaded", function() {
   try {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     if (!isSafari) {
