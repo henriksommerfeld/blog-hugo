5a6a3d155e364ce6c70c0cf4db16b1510508511c
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-11-12 14:26:09 +0100

    Lightbox module done

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import $ from 'jquery';
-
 export default class LightBox {
     constructor() {
         console.log('%c 🖼️ Lightbox module loaded', 'font-size:1.5em');
@@ -10,28 +8,26 @@ export default class LightBox {
 
         this.addOpenEvent();
         this.addCancelEvent();
-        this.addCloseEvent();
         this.addCloseOnEscEvent();
     }
 
     addCancelEvent() {
-        $('body').on('click touch', '#lightbox-loading', (e) => {
+        const cancel = event => {
             this.cancelShowImage = true;
-            this.closeModal(e);
-        });
-    }
-
-    addCloseEvent() {
-        $('body').on('click touchmove', '#lightbox img', (e) => {
-            this.closeModal(e);
-        });
+            this.closeModal(event);
+        };
+        document.getElementById('lightbox-loading').addEventListener('click', cancel);
+        document.getElementById('lightbox-loading').addEventListener('touch', cancel);
     }
 
     addCloseOnEscEvent() {
-        $(document).keyup((e) => {
-            if (e.key === 'Escape' && $('#lightbox-container').hasClass('open')) {
-                this.closeModal(e);
-            }
+        document.addEventListener("keyup", e => {
+            if (e.key !== 'Escape') return;
+
+            const container = document.getElementById("lightbox-container");
+            if (!container.classList.contains("open")) return;
+            
+            this.closeModal(e);            
         });
     }
 
@@ -44,56 +40,55 @@ export default class LightBox {
             const topOffset = imageElement.offsetTop + (imageElement.clientHeight / 2) - window.pageYOffset;
             const leftOffset = imageElement.offsetLeft + (imageElement.clientWidth / 2);
 
+            const lightbox = document.getElementById('lightbox');
+            lightbox.style.cssText = `transform-origin: ${leftOffset}px ${topOffset}px`;
+            const container = document.getElementById('lightbox-container');
+            container.classList.remove('close');
+            container.classList.add('open');
 
-
+            const img = lightbox.querySelector('img');
+            if (!img || img.getAttribute('src') !== imageUrl) {
+                img && img.remove();
+                setTimeout(() => {
+                    if (lightbox.querySelectorAll('img').length === 0)
+                        document.getElementById('lightbox-loading-container').style.display = 'flex';
+                }, 500);
+                let downloadingImage = new Image();
+                downloadingImage.onload = () => {
+                    if (!this.cancelShowImage) {
+                        document.getElementById('lightbox-loading-container').style.display = 'none';
+                        lightbox.append(downloadingImage);                        
+                        lightbox.classList.remove('close');
+                        lightbox.classList.add('open');
+                                    
+                        downloadingImage.addEventListener('click', this.closeModal);
+                        downloadingImage.addEventListener('touchmove', this.closeModal);
+                    }
+                };
+                downloadingImage.src = imageUrl;                
+            }
+            else {
+                lightbox.classList.remove('close');
+                lightbox.classList.add('open');
+            }
+            document.querySelector('body').classList.add('modal-open');
         };
 
 
         const figureAnchors = document.querySelectorAll('figure > a');
         figureAnchors.forEach(element => {
             element.addEventListener('click', openLightbox);
-        });
-
-        $('body').on('click', 'figure > a', (e) => {
-            // e.preventDefault();
-            // this.cancelShowImage = false;
-            // const imageElement = $(e.currentTarget).find('img');
-            const scrollTop = $(window).scrollTop();
-            const topOffset = $(imageElement).offset().top + ($(imageElement).outerHeight() / 2) - scrollTop;
-            const leftOffset = $(imageElement).offset().left + ($(imageElement).outerWidth() / 2);
-            const imageUrl = e.currentTarget.href;
-
-            $('#lightbox').css('transform-origin', leftOffset + 'px ' + topOffset + 'px');
-            $('#lightbox-container').removeClass('close').addClass('open');
-
-            if ($('#lightbox img').attr('src') !== imageUrl) {
-                $('#lightbox img').remove();
-                $('#lightbox img').attr('src', '');
-                setTimeout(() => {
-                    if ($('#lightbox').has('img'))
-                        $('#lightbox-loading-container').show();
-                }, 500);
-                let downloadingImage = new Image();
-                downloadingImage.onload = () => {
-                    if (!this.cancelShowImage) {
-                        $('#lightbox-loading-container').hide();
-                        $('#lightbox').append(downloadingImage);
-                        $('#lightbox').removeClass('close').addClass('open');
-                    }
-                };
-                downloadingImage.src = imageUrl;
-            }
-            else {
-                $('#lightbox').removeClass('close').addClass('open');
-            }
-            $('body').addClass('modal-open');
-        });
+        });       
     }
 
     closeModal(e) {
-        $('#lightbox-loading-container').hide();
-        $('#lightbox-container, #lightbox, #lightbox-loading').removeClass('open').addClass('close');
-        $('body').removeClass('modal-open');
+        document.getElementById('lightbox-loading-container').style.display = 'none';
+        const elements = document.querySelectorAll('#lightbox-container, #lightbox, #lightbox-loading');
+        elements.forEach(element => {
+            element.classList.remove('open');
+            element.classList.add('close');
+        })
+        document.querySelector('body').classList.remove('modal-open');
         e.preventDefault();
     }
 }
