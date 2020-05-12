5318627b9c44d2e756d071527653495d2a27703a
Henrik Sommerfeld <henrik@sommerfeld.nu>
2020-05-12 14:49:13 +0200

    Added search focus tests

--- cypress/integration/search.test.js
+++ cypress/integration/search.test.js
@@ -10,29 +10,43 @@ context('Search', () => {
     });
 
     it('Should open searchbox', ()=> {
-        cy.get('nav').findByText('Search').click().get('#search-input').should('be.visible')
-    })
+      cy.get('nav').findByText('Search').click().get('#search-input').should('be.visible')
+    })    
 
     const expectedPostTitle = 'Firmware Update Notifications for My Asus Router';
     it(`Should find post with title '${expectedPostTitle}' search for "asus firmware"`, ()=> {
-        cy.get('#search-input')
-          .wait(2000) // give some time for index file to download
-          .type('asus firmware')
-          .get('#search-output .result-list')
-          .within(() => {
-            cy.findByText(expectedPostTitle)
-            .should('be.visible')
-          })
+      cy.get('#search-input')
+        .wait(2000) // give some time for index file to download
+        .type('asus firmware')
+        .get('#search-output .result-list')
+        .within(() => {
+          cy.findByText(expectedPostTitle)
+          .should('be.visible')
+        })
+    })
+    
+    it(`Should set focus on first hit on enter`, ()=> {
+      cy.get('#search-input')
+        .type('{enter}')
+        .get('#search-output .result-list li:first a')
+        .should('have.focus')
+    })
+
+    it(`Should set focus on first hit on down-arrow`, ()=> {
+      cy.get('#search-input')
+        .type('{downarrow}')
+        .get('#search-output .result-list li:first a')
+        .should('have.focus')
     })
 
     it(`Should navigate to post when clicked`, ()=> {
-        cy.get('#search-output .result-list')
-          .within(() => {
-            cy.findByText(expectedPostTitle)
-            .click()
-            .url()
-            .should('equal', Cypress.config().baseUrl + `/firmware-update-notifications-for-my-asus-router/`)
-          })
+      cy.get('#search-output .result-list')
+        .within(() => {
+          cy.findByText(expectedPostTitle)
+          .click()
+          .url()
+          .should('equal', Cypress.config().baseUrl + `/firmware-update-notifications-for-my-asus-router/`)
+        })
     })
 
     it('Should close on ESC key', ()=> {
