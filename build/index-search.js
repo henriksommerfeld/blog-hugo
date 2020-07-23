871c3a92d38da0d770f8cce9007c4968d87a14be
Henrik Sommerfeld <henrik@sommerfeld.nu>
2020-07-23 22:18:01 +0200

    Finnished lunr post

--- build/index-search.js
+++ build/index-search.js
@@ -7,28 +7,28 @@ const path = require('path');
 const outputFilePathParameter = process.argv && process.argv.slice(2)[0];
 
 const inputFilePath = path.resolve(__dirname, '../public/data-to-index.json');
-const outputFilePath =  outputFilePathParameter || path.resolve(__dirname, '../public/search-index.json');
+const outputFilePath = outputFilePathParameter || path.resolve(__dirname, '../public/search-index.json');
 console.log('Reading ' + inputFilePath);
 const documentsToIndex = require(inputFilePath);
 const store = {};
 
 console.log('Indexing ' + inputFilePath);
 const searchIndex = lunr(function () {
-  this.ref('ref')
-  this.field('title', {boost:10}),
-  this.field('tags', {boost:5}),
-  this.field('content')
+    this.ref('ref')
+    this.field('title', { boost: 10 }),
+        this.field('tags', { boost: 5 }),
+        this.field('content')
 
-  documentsToIndex.forEach(function (doc) {
-    store[doc.ref] = {
-        'title': doc.title,
-        'summary': doc.summary,
-        'dateiso': doc.dateiso,
-        'dateformatted': doc.dateformatted
-    };
+    documentsToIndex.forEach(function (doc) {
+        store[doc.ref] = {
+            'title': doc.title,
+            'summary': doc.summary,
+            'dateiso': doc.dateiso,
+            'dateformatted': doc.dateformatted
+        };
 
-    this.add(doc)
-  }, this)
+        this.add(doc)
+    }, this)
 })
 
 console.log('Saving index at ' + outputFilePath);
@@ -38,14 +38,14 @@ const dataToSave = JSON.stringify({
     store: store
 });
 
-fs.unlink(outputFilePath, function(err){
+fs.unlink(outputFilePath, function (err) {
 
     if (err && err.code !== 'ENOENT')
         throw err;
 
-    const options = { flag : 'w' };
-    fs.writeFile(outputFilePath, dataToSave, options, function(err) {
-        if (err) 
+    const options = { flag: 'w' };
+    fs.writeFile(outputFilePath, dataToSave, options, function (err) {
+        if (err)
             console.error(err);
         else
             console.log('Saved index at ' + outputFilePath);
